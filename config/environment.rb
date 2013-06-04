6ed3e90105fe2fbc0d793c3b9a5c63beba78d669
Eric <eric@swellny.com>
2013-06-04 12:16:36 -0400

    Email adjustments

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Artrequest::Application.initialize!
+
+
+ActionMailer::Baser.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
